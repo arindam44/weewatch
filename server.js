108c2d4a9a15b9faf67177534f2e43d9cc97a4cc
Arindam Roy <43511733+arindam44@users.noreply.github.com>
2020-12-16 18:54:38 +0530

    removed express.static

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const port = process.env.PORT || 6000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/build")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "client/build")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "client/build/index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.join(__dirname, "client/build/index.html"));
+// 	});
+// }
 
 httpServer.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
