71ac76752c7f6259c6d60f51ac461f7636378e48
Arindam Roy <43511733+arindam44@users.noreply.github.com>
2020-12-26 00:25:59 +0530

    added * route in server for heroku routing issue

--- server.js
+++ server.js
@@ -54,7 +54,11 @@ app.use(cors());
 // });
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/build")));
+	app.use(express.static("client/build"));
+
+	app.get("*", (request, response) => {
+		response.sendFile(path.join(__dirname, "client/build", "index.html"));
+	});
 }
 
 var rooms = [];
